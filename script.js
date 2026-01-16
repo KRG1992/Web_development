97ed55809238f4ff86853f227f85a27e19517c59
ElChurro1992 <greene983@gmail.com>
2026-01-16 12:51:02 -0700

    Initial commit message

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function formValidaition() {
         alert("All fields must be filled out");
         return false;
     }
-    alert("Thank you for your information " + name + "!");
+    alert(`Thank you for your information ${name}!`);
     return true;
 }
+
+const dateInput = document.getElementById("date-input");
+
+dateInput.addEventListener("change", (event) => {
+
+    const dateString = event.target.value;
+    console.log("Date string: ", dateString);
+
+    const dateObject = new Date(`${dateString}T00:00`);
+    console.log("Date object: ", dateObject);
+})
